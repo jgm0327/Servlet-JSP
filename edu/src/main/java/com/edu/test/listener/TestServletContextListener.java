f426b2669613a0ff4eba549c7f20ba8e1f2cb56d
jgm0327 <gmjang6349@gmail.com>
2022-10-10 18:17:57 +0900

    Listener 구현
    SerlvetRequest, Session, ServletContext 의 listener 들을 구현해 봄

---
+++ edu/src/main/java/com/edu/test/listener/TestServletContextListener.java
@@ -0,0 +1,23 @@
+package com.edu.test.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class TestServletContextListener implements ServletContextListener{
+
+	public TestServletContextListener() {
+		System.out.println("TestSerlvetContextListener 객체 생성");
+	}
+	
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("ServletContext 객체 초기화");
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("ServletContext 객체 해제");
+	}
+}
